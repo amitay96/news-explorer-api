8bf71fa2cae9596a9df5de496ac972b60a8eacc7
amitay96 <amitay96@gmail.com>
2022-10-29 14:15:32 +0300

    feature: add joi validation

---
+++ middlewares/validation.js
@@ -0,0 +1,57 @@
+const { celebrate, Joi } = require("celebrate");
+const { ObjectId } = require("mongoose").Types;
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+const validateAuth = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().message("Valid email is required"),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+const validateUser = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
+const validateArticle = celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().custom(validateURL),
+    image: Joi.string().required().custom(validateURL),
+  }),
+});
+
+const validateId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        if (ObjectId.isValid(value)) {
+          return value;
+        }
+        return helpers.message("Invalid id");
+      }),
+  }),
+});
+
+module.exports = {
+  validateAuth,
+  validateUser,
+  validateArticle,
+  validateId,
+};
